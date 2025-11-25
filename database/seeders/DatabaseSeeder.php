1614b883b1171f7a450d624bc65abb3d34adf8c3
edwardscrints <edwardscrints@gmail.com>
2025-11-25 01:44:51 -0500

    [SOL_001] Script seeder para poblar BD

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // Crear usuarios de prueba
+        \App\Models\User::factory()->create([
+            'name' => 'AdminGabriel',
+            'email' => 'edward.gabriel@grupooet.com',
+            'password' => bcrypt('password'),
+        ]);
+        
+        \App\Models\User::factory(5)->create();
+
+        \App\Models\Third::factory(60)->create();
+
+        \App\Models\Merchandise::factory(90)->create();
+
+        \App\Models\Shipment::factory(20)->create();
+
+        $this->command->info('Base de datos poblada exitosamente!');
     }
 }
