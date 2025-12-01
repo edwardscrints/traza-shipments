05d72e94f914ca9f77725c8057ba9c65626ef102
edwardscrints <edwardscrints@gmail.com>
2025-12-01 00:21:31 -0500

    [SOl_001] Se deja comentado datos para v2

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,7 +20,11 @@ class User extends Authenticatable
     protected $fillable = [
         'name',
         'email',
-        'password',
+        'password'
+        //age
+        //address
+        //latitud 
+        //longitud
     ];
 
     /**
